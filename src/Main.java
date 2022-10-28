30ec493168de68e28693477d9a050e451df1f61c
Anchel17 <80929766+Anchel17@users.noreply.github.com>
2022-10-28 17:00:14 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,56 +14,43 @@ public class Main {
 	public static void main(String[] args) {
 		No raiz = new No(20);
 		
+		No.inserir(raiz);
+		
 		No no10 = new No(10);
-		raiz.noEsquerdo = no10;
+		//raiz.noEsquerdo = no10;
+		No.inserir(no10);
 		
 		No no15 = new No(15);
-		no10.noDireito = no15;
+		//no10.noDireito = no15;
+		No.inserir(no15);
 		
 		No no40 = new No(40);
-		raiz.noDireito = no40;
+		//raiz.noDireito = no40;
+		No.inserir(no40);
 		
 		No no30 = new No(30);
-		no40.noEsquerdo = no30;
+		//no40.noEsquerdo = no30;
+		No.inserir(no30);
 		
 		No no50 = new No(50);
-		no40.noDireito = no50;
+		//no40.noDireito = no50;
+		No.inserir(no50);
 		
 		No no60 = new No(60);
-		no50.noDireito = no60;
+		//no50.noDireito = no60;
+		No.inserir(no60);
+
+		//No.inserir(new No(29));
+		//No.inserir(new No(22));
+		No.inserir(new No(19));
+		No.inserir(new No(18));
+		No.remover(raiz);
 		
-		raiz.inserir(new No(31), raiz);
-		raiz.inserir(new No(32), raiz);
+		//No.inserir(new No(31));
+		//No.inserir(new No(32));
 		//System.out.println(raiz.busca(15, raiz));
-		System.out.println(raiz.remover(no40, raiz));
-		//System.out.println(raiz.busca(30, no15));
-		preOrdem(raiz);
+		//System.out.println(raiz.remover(no40));
+		//System.out.println(No.busca(60));
+		preOrdem(No.getRaiz());
 	}
 }
-
-/*
-No no14 = new No(14);
-No no9 = new No(9);
-No no31 = new No(31);
-
-System.out.println(raiz.inserir(no14, raiz));
-System.out.println(raiz.inserir(no9, raiz));
-System.out.println(raiz.inserir(no31, raiz));
-System.out.println(raiz.busca(31, raiz));
-*/
-
-/*Testes de inserção, remoção e busca
-No no13 = new No(13);
-raiz.inserir(no13, raiz);
-
-No no33 = new No(33);
-raiz.inserir(no33, raiz);
-
-No no39 = new No(39);
-raiz.inserir(no39, raiz);
-
-System.out.println(raiz.busca(no39.valor, raiz));
-*/
-//System.out.println(raiz.busca(no15.valor, raiz));
-//System.out.println(raiz.remover(no30, raiz));
-//System.out.println(raiz.busca(no39.valor, raiz));
