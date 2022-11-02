2597176dd5ec8c84acd0caa40dbc499f6931d6bf
Danrley Lima <danrleylac@gmail.com>
2022-11-02 10:52:41 -0300

    Lendo arquivos

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+	public static void arvoreEntrada(String arquivo) throws FileNotFoundException {
+		File arvoreEntrada = new File(arquivo);
+		Scanner sc = new Scanner(arvoreEntrada);
+		sc.useDelimiter(" ");
+		while (sc.hasNext()) {
+			int linha = Integer.parseInt(sc.next());
+			No.inserir(new No(linha));
+		}
+		sc.close();
+	}
+
 	public static void preOrdem(No raiz) {
 		System.out.println(raiz.valor);
 
@@ -23,17 +38,18 @@ public class Main {
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		arvoreEntrada(args[0]);
 
-		No.inserir(new No(50));
-		No.inserir(new No(70));
-		No.inserir(new No(35));
-		No.inserir(new No(40));
-		No.inserir(new No(25));
-		No.inserir(new No(30));
-		No.inserir(new No(65));
-		No.inserir(new No(90));
-		No.inserir(new No(80));
+		// No.inserir(new No(50));
+		// No.inserir(new No(70));
+		// No.inserir(new No(35));
+		// No.inserir(new No(40));
+		// No.inserir(new No(25));
+		// No.inserir(new No(30));
+		// No.inserir(new No(65));
+		// No.inserir(new No(90));
+		// No.inserir(new No(80));
 
 		/*
 		 * Testes árvore cheia.
@@ -49,9 +65,10 @@ public class Main {
 		 * No.getRaiz().noDireito.valor);
 		 */
 
-		System.out.println(No.remover(35));
+		// System.out.println(No.remover(35));
+
 		System.out.println("Altura da raiz: " + No.calcAltura(No.getRaiz()));
-		preOrdem(No.getRaiz());
+		// preOrdem(No.getRaiz());
 		inOrdem(No.getRaiz());
 		System.out.println("\nElemento n: ");
 		No.enesimoElemento(No.getRaiz(), 5);
